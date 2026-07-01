f1fd9a4cf04737a8b477b41eef489a344d5422ac
Victor <victorhanield@gmail.com>
2026-07-01 14:31:13 -0400

    Capitulos 7 y 8

---
+++ PG7.13/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Calcula longitud.
+El programa calcula la longitud de la cadena sin utilizar la función strlen. */
+
+int cuenta(char *);
+
+void main(void)
+{
+    int i;
+    char cad[50];
+    printf("\nIngrese la cadena de caracteres: ");
+    gets(cad);
+    i = cuenta(cad);
+    printf("\nLongitud de la cadena: %d", i);
+}
+
+int cuenta(char *cadena)
+{
+    int c = 0;
+    while (!cadena[c] == '\0')
+        c++;
+    return(c);
+}
